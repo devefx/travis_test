10863f69a8c33ff9028dda6cb2aad0ba9f56913d
Yoke <devefx@163.com>
2019-01-26 02:23:46 +0800

    Update before-install.sh

--- travis-scripts/before-install.sh
+++ travis-scripts/before-install.sh
@@ -12,6 +12,12 @@ echo "====================${VS140COMNTOOLS}"
 
 cd ${VS140COMNTOOLS}
 
+if [ -d $VS140COMNTOOLS ] ; then
+  echo "====================${VS140COMNTOOLS}"
+elif
+  echo "--------------------${VS140COMNTOOLS}"
+fi
+
 pwd
 
 ls
