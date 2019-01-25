712a8b0f0f328e34bd2ff674dc7e7f393a235536
Yoke <devefx@163.com>
2019-01-26 02:36:12 +0800

    Update before-install.sh

--- travis-scripts/before-install.sh
+++ travis-scripts/before-install.sh
@@ -10,7 +10,7 @@ env
 
 echo "====================${VS140COMNTOOLS}"
 
-cd ${VS140COMNTOOLS}
+cd /c/Program Files (x86)
 
 if [ -d $VS140COMNTOOLS ] ; then
   echo "====================${VS140COMNTOOLS}"
