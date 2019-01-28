a75597a310413a8470e8caaf6574a122efe33fa2
Yoke <devefx@163.com>
2019-01-28 11:20:44 +0800

    Update before-install.sh

--- travis-scripts/before-install.sh
+++ travis-scripts/before-install.sh
@@ -14,22 +14,33 @@ echo "====================${VS140COMNTOOLS}"
 cd /c/progra~2
 
 ls
-echo "===================="
-ls -l "/c/Program Files (x86)/MSBuild"
-echo "===================="
-ls -l "/c/Program Files (x86)/Microsoft Visual Studio"
-echo "===================="
-ls -l "/c/Program Files (x86)/Microsoft Visual Studio 14.0"
 
+echo "====================MSBuild"
+cd "/c/Program Files (x86)/MSBuild"
+ls
 echo "====================MSBuild/15.0"
-ls -l "/c/Program Files (x86)/MSBuild/15.0"
-echo "====================Microsoft Visual Studio/2017"
-ls -l "/c/Program Files (x86)/Microsoft Visual Studio/2017"
-echo "====================Microsoft Visual Studio 14.0/Common7"
-ls -l "/c/Program Files (x86)/Microsoft Visual Studio 14.0/Common7"
-
 cd "/c/Program Files (x86)/MSBuild/15.0"
 ls
+
+echo "====================Microsoft Visual Studio"
+cd "/c/Program Files (x86)/Microsoft Visual Studio"
+ls
+echo "====================Microsoft Visual Studio/2017"
+cd "/c/Program Files (x86)/Microsoft Visual Studio/2017"
+ls
+
+
+echo "====================Microsoft Visual Studio 14.0"
+cd "/c/Program Files (x86)/Microsoft Visual Studio 14.0"
+ls
+echo "====================Microsoft Visual Studio 14.0/Common7"
+cd "/c/Program Files (x86)/Microsoft Visual Studio 14.0/Common7"
+ls
+echo "====================Microsoft Visual Studio 14.0/VC"
+cd "/c/Program Files (x86)/Microsoft Visual Studio 14.0/VC"
+ls
+
+
 
 if [ -d "/c/Program Files (x86)/MSBuild/15.0/bin" ] ; then
   cd "/c/Program Files (x86)/MSBuild/15.0/bin"
