add123eab8426e8a472a7c17863e14e7f9dc0fd8
Yoke <devefx@163.com>
2019-01-31 02:26:44 +0800

    Update run-script.sh

--- travis-scripts/run-script.sh
+++ travis-scripts/run-script.sh
@@ -12,17 +12,16 @@ echo "Download ${SETUPTOOLS_DOWNLOAD_URL}"
 curl -O ${SETUPTOOLS_DOWNLOAD_URL}
 unzip "setuptools-40.7.1.zip" > /dev/null
 cd "setuptools-40.7.1"
-ls
 python setup.py install
 echo "setuptools-40.7.1 installed!"
 
-cd ..
+#cd ..
 
-PIP_DOWNLOAD_URL="https://files.pythonhosted.org/packages/c8/89/ad7f27938e59db1f0f55ce214087460f65048626e2226531ba6cb6da15f0/pip-19.0.1.tar.gz"
-echo "Download ${PIP_DOWNLOAD_URL}"
-curl -O ${PIP_DOWNLOAD_URL}
-tar -zxf "pip-19.0.1.tar.gz"
-cd "pip-19.0.1"
+#PIP_DOWNLOAD_URL="https://files.pythonhosted.org/packages/c8/89/ad7f27938e59db1f0f55ce214087460f65048626e2226531ba6cb6da15f0/pip-19.0.1.tar.gz"
+#echo "Download ${PIP_DOWNLOAD_URL}"
+#curl -O ${PIP_DOWNLOAD_URL}
+#tar -zxf "pip-19.0.1.tar.gz"
+#cd "pip-19.0.1"
 #python setup.py install
 
 
