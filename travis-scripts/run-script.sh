f3ee212d6df211f39a6e957a44e8f652f8d25e56
Yoke <devefx@163.com>
2019-01-31 01:56:32 +0800

    Update run-script.sh

--- travis-scripts/run-script.sh
+++ travis-scripts/run-script.sh
@@ -5,22 +5,19 @@ pushd $HOME/bin
 SETUPTOOLS_DOWNLOAD_URL="https://files.pythonhosted.org/packages/3b/17/81a4ab4c94e9c78f98dfaad0208994129213089361dc53a7cd8de177d1e3/setuptools-40.7.1.zip"
 echo "Download ${SETUPTOOLS_DOWNLOAD_URL}"
 curl -O ${SETUPTOOLS_DOWNLOAD_URL}
-ls
 unzip "setuptools-40.7.1.zip" -d "setuptools-40.7.1"  > /dev/null
-ls
 cd "setuptools-40.7.1"
 python setup.py install
-
-echo "Inatll"
+echo "setuptools-40.7.1 installed!"
 
 cd ..
 
-PIP_VERSION="19.0.1"
-PIP_DOWNLOAD_URL="https://files.pythonhosted.org/packages/c8/89/ad7f27938e59db1f0f55ce214087460f65048626e2226531ba6cb6da15f0/pip-${PIP_VERSION}.tar.gz"
+PIP_DOWNLOAD_URL="https://files.pythonhosted.org/packages/c8/89/ad7f27938e59db1f0f55ce214087460f65048626e2226531ba6cb6da15f0/pip-19.0.1.tar.gz"
 echo "Download ${PIP_DOWNLOAD_URL}"
 curl -O ${PIP_DOWNLOAD_URL}
-tar -zxf "pip-${PIP_VERSION}.tar.gz"
-cd "pip-${PIP_VERSION}"
+tar -zxf "pip-19.0.1.tar.gz"
+ls
+cd "pip-19.0.1"
 python setup.py install
 
 popd
